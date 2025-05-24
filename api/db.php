8b0f5468c1dc4f63e5ca1e6afabc1c5155db2f59
row-huh <rohaabropathan@gmail.com>
2025-05-24 15:01:13 +0500

    db.php updated

--- api/db.php
+++ api/db.php
@@ -1,1 +1,19 @@
 //TODO : Access database
+
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db_name = "news_data";
+
+
+$conn = new sqli($servername, $username, $password, $db_name);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected Successfully"
+
+?>
